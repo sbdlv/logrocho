8d184e34857fa4091107daa935e38a21077906a7
sbdlv <58570190+sbdlv@users.noreply.github.com>
2022-01-26 09:19:37 +0100

    Definido funcionamiento libreria js para crear las peticiones automáticas de modificación de tablas

---
+++ DWEC/js/AutoTables.js
@@ -0,0 +1,4 @@
+// data-propId -> El nombre de la propiedad de la entidad
+// data-propCheckMethod -> El nombre de la funcion que comprobara la validez del campo
+
+// Las tablas deben tener la clase .autotable en el tag <table>
