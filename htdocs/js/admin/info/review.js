02e1632af79e3f319e5132d992975cd5eea0dece
sbdlv <sergiobarriodelavega@gmail.com>
2022-03-07 13:48:46 +0100

    Añadida comprobación de campos para las reseñas

--- htdocs/js/admin/info/review.js
+++ htdocs/js/admin/info/review.js
@@ -1,4 +1,7 @@
 $("#save_btn").on("click", () => {
+    if (!checkFields()) {
+        return;
+    }
     let data = {
         id: $("#review_id").val(),
         user_id: $("#review_user_id").val(),
@@ -24,6 +27,51 @@ $("#save_btn").on("click", () => {
 })
 
 
-$("#open_pincho").on("click", ()=>{
+$("#open_pincho").on("click", () => {
     window.location.href = `index.php/pincho/edit/${$("#review_pincho_id").val()}`;
 })
+
+
+function checkFields() {
+    if ($("#review_title").val() === "") {
+        alert("El campo titulo es obligatorio");
+        return false;
+    }
+
+    if ($("#review_desc").val() === "") {
+        alert("El campo descripción es obligatorio");
+        return false;
+    }
+
+    if ($("#review_taste").val() === "") {
+        alert("El campo sabor es obligatorio");
+        return false;
+    }
+
+    if ($("#review_texture").val() === "") {
+        alert("El campo textura es obligatorio");
+        return false;
+    }
+
+    if ($("#review_presentation").val() === "") {
+        alert("El campo presentación es obligatorio");
+        return false;
+    }
+
+    if (parseInt($("#review_taste").val()) > 5 || parseInt($("#review_taste").val()) < 0) {
+        alert("El campo sabor solo admite valores entre 0 y 5 incluidos");
+        return false;
+    }
+
+    if (parseInt($("#review_texture").val()) > 5 || parseInt($("#review_texture").val()) < 0) {
+        alert("El campo textura solo admite valores entre 0 y 5 incluidos");
+        return false;
+    }
+
+    if (parseInt($("#review_presentation").val()) > 5 || parseInt($("#review_presentation").val()) < 0) {
+        alert("El campo presentación solo admite valores entre 0 y 5 incluidos");
+        return false;
+    }
+
+    return true
+}
